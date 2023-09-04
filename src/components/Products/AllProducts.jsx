2faa76d8320c1fafd45522c08ce8217b7cd8a3da
gaurav <gauravsoni8414@gmail.com>
2023-09-04 21:25:28 +0530

    basic ui set

--- src/components/Products/AllProducts.jsx
+++ src/components/Products/AllProducts.jsx
@@ -5,7 +5,7 @@ const AllProducts = () => {
   return (
     <div>
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 flex items-start justify-center gap-4 flex-wrap">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 flex items-start justify-start gap-3 flex-wrap">
           <SingleProduct />
         </div>
       </div>
