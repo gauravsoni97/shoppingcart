c2a142318d3cff76f30cbac5eda7ae80019b81c8
gaurav <gauravsoni8414@gmail.com>
2023-09-10 20:49:22 +0530

    check mark rating clear filter set

--- src/components/Filters/Rating/StarRatingUi.jsx
+++ src/components/Filters/Rating/StarRatingUi.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import { useProductsContext } from "../../../context/ProductsContext";
 
 const StarRatingUi = ({ StarRatingValue, setSelectedRating }) => {
+  const { clearAllFilters, setClearAllFilters, setFilteredProducts, products } =
+    useProductsContext();
+
   const [checkedStar, setCheckedStar] = useState(false);
 
   const handleCheckedValue = () => {
     const newCheckedState = !checkedStar;
     setCheckedStar(newCheckedState);
+    setClearAllFilters(false);
 
     if (newCheckedState) {
       setSelectedRating(StarRatingValue);
@@ -16,6 +21,13 @@ const StarRatingUi = ({ StarRatingValue, setSelectedRating }) => {
 
     console.log("checkedStar", checkedStar);
   };
+  useEffect(() => {
+    if (clearAllFilters) {
+      setSelectedRating(null);
+      setCheckedStar(false);
+      setFilteredProducts(products);
+    }
+  }, [clearAllFilters]);
 
   return (
     <div className="flex items-center mb-2">
@@ -25,9 +37,7 @@ const StarRatingUi = ({ StarRatingValue, setSelectedRating }) => {
         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         onChange={handleCheckedValue}
       />
-      <label
-        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 flex items-center"
-      >
+      <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 flex items-center">
         {StarRatingValue}
         <StarRoundedIcon
           style={{ color: "yellow", fontSize: "1rem", margin: "0 .2rem" }}
