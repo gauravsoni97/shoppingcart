39a820b6d240c4166930ae4abcd33322d1e10a0e
gaurav <gauravsoni8414@gmail.com>
2023-09-10 11:44:57 +0530

    rating functionality fixed

--- src/components/Products/AllProducts.jsx
+++ src/components/Products/AllProducts.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import SingleProduct from "./SingleProduct";
 import { useProductsContext } from "../../context/ProductsContext";
 import SkeletonProductCard from "./SkeletonProductCard";
+import NoResults from "./NoResults";
 
 const AllProducts = () => {
   const { filteredProducts, loading, error } = useProductsContext();
 
   return (
     <div className="p-4 sm:ml-64">
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 flex items-start justify-start gap-3 flex-wrap">
-        {filteredProducts.length === 0 && !loading && <p>no data found</p>}
+      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-20 flex items-start justify-start gap-3 flex-wrap">
+        {filteredProducts.length === 0 && !loading && <NoResults />}
         {loading && <SkeletonProductCard />}
         {(filteredProducts || []).map((product) => (
           <SingleProduct key={product.id} product={product} />
---
+++ src/components/Products/NoResults.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoResults = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh] w-full">
+      No Results Found
+    </div>
+  );
+};
+
+export default NoResults;
