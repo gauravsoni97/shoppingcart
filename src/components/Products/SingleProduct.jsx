7aa533fafe0029b4a82aa6b4db17b785bb7a7dc1
gaurav <gauravsoni8414@gmail.com>
2023-09-16 21:52:59 +0530

    add to cart item set

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -31,7 +31,8 @@ const SingleProduct = ({ product }) => {
               ${price}
             </span>
 
-            <button onClick={( )=>addToCartItem(id)}
+            <button
+              onClick={() => addToCartItem(id)}
               type="button"
               className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
