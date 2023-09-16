3e651c3e91e1e69b99ee9c6a6ae25141716de5dc
gaurav <gauravsoni8414@gmail.com>
2023-09-16 14:24:15 +0530

    search functionality done

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,35 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import { useProductsContext } from "../../context/ProductsContext";
 
 const SearchBar = () => {
+  const { setFilteredProducts, products } = useProductsContext();
+
+  const [searchItem, setSearchItem] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loadingSuggestions, setLoadingSuggestions] = useState(false);
+  const [suggestionSelected, setSuggestionSelected] = useState(false); // Track suggestion selection
+
+  useEffect(() => {
+    const debounceTimer = setTimeout(() => {
+      const updatedList = products.filter((product) =>
+        product.title.toLowerCase().includes(searchItem.toLowerCase())
+      );
+      setLoadingSuggestions(false);
+      setFilteredProducts(updatedList);
+      setSuggestions(updatedList.map((product) => product.title));
+    }, 500);
+
+    setLoadingSuggestions(true);
+
+    return () => {
+      clearTimeout(debounceTimer);
+    };
+  }, [searchItem, products, setFilteredProducts]);
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // Function to handle suggestion selection
+  const handleSuggestionClick = (selectedSuggestion) => {
+    setSearchItem(selectedSuggestion);
+    setSuggestionSelected(true);
+  };
+
   return (
     <div className="w-[40vw]">
-      <form>
-        <label
-          for="default-search"
-          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
-        </label>
-        <div class="relative">
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+      <form onSubmit={handleSearchSubmit}>
+        <div className="relative">
+          <button
+            type="submit"
+            className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
             <SearchRoundedIcon />
-          </div>
+          </button>
           <input
             type="search"
             id="default-search"
-            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search Mockups, Logos..."
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search product here..."
             required
+            value={searchItem}
+            autoComplete="off"
+            onChange={(e) => setSearchItem(e.target.value)}
           />
-          <button
-            type="submit"
-            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Search
-          </button>
         </div>
       </form>
+
+      {searchItem.length > 0 &&
+        !suggestionSelected &&
+        suggestions.length > 0 && (
+          <ul className="absolute  bg-gray-800 w-[30vw] h-36 overflow-auto rounded-lg p-6 ">
+            {loadingSuggestions ? (
+              <li>Loading...</li>
+            ) : (
+              suggestions.map((suggest, index) => (
+                <li
+                  className="mb-4 cursor-pointer"
+                  key={index}
+                  onClick={() => handleSuggestionClick(suggest)}
+                >
+                  {suggest}
+                </li>
+              ))
+            )}
+          </ul>
+        )}
     </div>
   );
 };
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,11 +1,11 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const CartCtx = createContext();
 
 const CartContext = ({ children }) => {
+const [addtoCartList, setaddtoCartList] = useState([])
 
-    
-  const ctxValues = {};
+  const ctxValues = {addtoCartList, setaddtoCartList};
 
   return <CartCtx.Provider value={ctxValues}>{children}</CartCtx.Provider>;
 };
