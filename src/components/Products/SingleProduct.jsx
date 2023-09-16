4c420ceb4475b464b5b1a69db4a52abb45080b47
gaurav <gauravsoni8414@gmail.com>
2023-09-16 21:52:17 +0530

    add to cart functionality added

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { useCartContext } from "../../context/CartContext";
 
-const SingleProduct = ({product }) => {
-  const { images, title, description, rating, price } = product;
+const SingleProduct = ({ product }) => {
+  const { id, images, title, description, rating, price } = product;
+
+  const { addToCartItem } = useCartContext();
+
   return (
     <div>
       <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <img className="p-8 rounded-lg h-72 w-96 object-cover" src={images[0]} alt="product image" />
+        <img
+          className="p-8 rounded-lg h-72 w-96 object-cover"
+          src={images[0]}
+          alt="product image"
+        />
         <div className="px-5 pb-5">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-              {title}
-            </h5>
-            <p className="text-sm tracking-tight text-gray-900 dark:text-white line-clamp-[1]">
-              {description}
-            </p>
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+          <p className="text-sm tracking-tight text-gray-900 dark:text-white line-clamp-[1]">
+            {description}
+          </p>
           <div className="flex items-center mt-2.5 mb-5">
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold  px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ">
-              Rating: {(rating).toFixed(1)}
+              Rating: {rating.toFixed(1)}
             </span>
           </div>
           <div className="flex items-center justify-between">
@@ -23,8 +31,12 @@ const SingleProduct = ({product }) => {
               ${price}
             </span>
 
-            <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to Cart</button>
-
+            <button onClick={( )=>addToCartItem(id)}
+              type="button"
+              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,13 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { useProductsContext } from "./ProductsContext";
 
 export const CartCtx = createContext();
 
 const CartContext = ({ children }) => {
-const [addtoCartList, setaddtoCartList] = useState([])
+  const [addtoCartList, setaddtoCartList] = useState([]);
 
-  const ctxValues = {addtoCartList, setaddtoCartList};
+  const { products } = useProductsContext();
+
+  const addToCartItem = (id) => {
+    const AddThisItem = products.filter((ele) => {
+      return ele.id === id;
+    });
+
+    setaddtoCartList([...addtoCartList, AddThisItem]);
+  };
+
+  console.log("addtoCartList", addtoCartList);
+
+  const ctxValues = { addToCartItem };
 
   return <CartCtx.Provider value={ctxValues}>{children}</CartCtx.Provider>;
 };
 
 export default CartContext;
+
+// Custom hook to access the context value
+export const useCartContext = () => {
+  return useContext(CartCtx);
+};
