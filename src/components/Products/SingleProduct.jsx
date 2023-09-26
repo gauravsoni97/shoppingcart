33dd540a6f344bc6dd2711278f7ba32dc5f39b00
gaurav <gauravsoni8414@gmail.com>
2023-09-26 21:41:32 +0530

    product details page half done

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -12,49 +12,49 @@ const SingleProduct = ({ product }) => {
   return (
     <div>
       <Link to={`/product/${product.id}`}>
-      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img
-          className="p-8 rounded-lg h-72 w-96 object-cover"
-          src={images[0]}
-          alt="product image"
-        />
-        <div className="px-5 pb-5">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-          <p className="text-sm tracking-tight text-gray-900 dark:text-white line-clamp-[1]">
-            {description}
-          </p>
-          <div className="flex items-center mt-2.5 mb-5">
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold  px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ">
-              Rating: {rating.toFixed(1)}
-            </span>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              ${price}
-            </span>
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img
+            className="p-8 rounded-lg h-72 w-96 object-cover"
+            src={images[0]}
+            alt="product image"
+          />
+          <div className="px-5 pb-5">
+            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              {title}
+            </h5>
+            <p className="text-sm tracking-tight text-gray-900 dark:text-white line-clamp-[1]">
+              {description}
+            </p>
+            <div className="flex items-center mt-2.5 mb-5">
+              <span className="bg-blue-100 text-blue-800 text-xs font-semibold  px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ">
+                Rating: {rating.toFixed(1)}
+              </span>
+            </div>
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                ${price}
+              </span>
 
-            {isInList ? (
-              <button
-                onClick={() => removeCartItem(id)}
-                type="button"
-                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Remove From Cart
-              </button>
-            ) : (
-              <button
-                onClick={() => addToCartItem(id)}
-                type="button"
-                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Add to Cart
-              </button>
-            )}
+              {isInList ? (
+                <button
+                  onClick={() => removeCartItem(id)}
+                  type="button"
+                  className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Remove From Cart
+                </button>
+              ) : (
+                <button
+                  onClick={() => addToCartItem(id)}
+                  type="button"
+                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Add to Cart
+                </button>
+              )}
+            </div>
           </div>
         </div>
-      </div>
       </Link>
     </div>
   );
