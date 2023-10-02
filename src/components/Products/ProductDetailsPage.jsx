12621929b967a8a39eb93d0fed1f08c6db677707
gaurav <gauravsoni8414@gmail.com>
2023-10-02 20:33:44 +0530

    product details page udated

--- src/components/Products/ProductDetailsPage.jsx
+++ src/components/Products/ProductDetailsPage.jsx
@@ -2,215 +2,109 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useProductsContext } from "../../context/ProductsContext";
 
+// Define a separate Icon component to reduce duplication
+const Icon = () => (
+  <svg
+    fill="currentColor"
+    stroke="currentColor"
+    stroke-linecap="round"
+    strokeLineJoin="round"
+    strokeWidth="2"
+    className="w-4 h-4 text-blue-400"
+    viewBox="0 0 24 24"
+  >
+    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+  </svg>
+);
+
 const ProductDetailsPage = () => {
   const { id: SelectedProductId } = useParams();
-
   const { filteredProducts } = useProductsContext();
-
   const [selectedProductItem, setSelectedProductItem] = useState(null);
 
   useEffect(() => {
     const selectedProduct = filteredProducts.find(
       (ele) => ele.id === parseInt(SelectedProductId)
     );
-
     setSelectedProductItem(selectedProduct);
   }, [SelectedProductId, filteredProducts]);
 
-  console.log("selectedProductItem", selectedProductItem);
-  console.log("SelectedProductId", selectedProductItem);
-
   return (
-    <div class="text-gray-400  absolute top-16 body-font ">
-      {selectedProductItem && 
-          <section class="text-gray-400 body-font">
-            <div class="container px-5 py-24 mx-auto">
-              <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                <div class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
-                  <div class="grid gap-4">
-                    <div>
-                      <img
-                        class="h-auto max-w-full rounded-lg"
-                        src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg"
-                        alt=""
-                      />
-                    </div>
-                    <div class="grid grid-cols-5 gap-4">
-                      <div>
+    <div className="text-gray-400 absolute top-16 body-font">
+      {selectedProductItem && (
+        <section className="text-gray-400 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+              <div className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded">
+                <div className="grid gap-4">
+                  <div
+                    className="h-80 max-w-full rounded-lg bg-cover bg-no-repeat bg-center"
+                    style={{
+                      backgroundImage: `url(${selectedProductItem.images[0]})`,
+                    }}
+                  ></div>
+                  <div className="grid grid-cols-5 gap-4">
+                    {selectedProductItem.images.slice(1, 5).map((image, index) => (
+                      <div key={index}>
                         <img
-                          class="h-auto max-w-full rounded-lg"
-                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                          className="h-20 max-w-full rounded-lg"
+                          src={image}
                           alt=""
                         />
                       </div>
-                      <div>
-                        <img
-                          class="h-auto max-w-full rounded-lg"
-                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-                          alt=""
-                        />
-                      </div>
-                      <div>
-                        <img
-                          class="h-auto max-w-full rounded-lg"
-                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-                          alt=""
-                        />
-                      </div>
-                      <div>
-                        <img
-                          class="h-auto max-w-full rounded-lg"
-                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-                          alt=""
-                        />
-                      </div>
-                      <div>
-                        <img
-                          class="h-auto max-w-full rounded-lg"
-                          src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-                          alt=""
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                  <h2 class="text-sm title-font text-gray-500 tracking-widest">
-                    {selectedProductItem.brand}
-                  </h2>
-                  <h1 class="text-white text-3xl title-font font-medium mb-1">
-                   {selectedProductItem.title}
-                  </h1>
-                  <div class="flex mb-4">
-                    <span class="flex items-center">
-                      <svg
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-4 h-4 text-blue-400"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                      </svg>
-                      <svg
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-4 h-4 text-blue-400"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                      </svg>
-                      <svg
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-4 h-4 text-blue-400"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                      </svg>
-                      <svg
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-4 h-4 text-blue-400"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                      </svg>
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-4 h-4 text-blue-400"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                      </svg>
-                      <span class="ml-3">{selectedProductItem.rating} Rating</span>
-                    </span>
-                    <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-800 text-gray-500 space-x-2">
-                      <a>
-                        <svg
-                          fill="currentColor"
-                          stroke-linecap="round"
-                          strokeLineJoin="round"
-                          strokeWidth="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                        </svg>
-                      </a>
-                      <a>
-                        <svg
-                          fill="currentColor"
-                          stroke-linecap="round"
-                          strokeLineJoin="round"
-                          strokeWidth="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                        </svg>
-                      </a>
-                      <a>
-                        <svg
-                          fill="currentColor"
-                          stroke-linecap="round"
-                          strokeLineJoin="round"
-                          strokeWidth="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                      </a>
-                    </span>
-                  </div>
-                  <p class="leading-relaxed">
-                    {selectedProductItem.description}
-                  </p>
-                  <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5">
-            
-                 
-                  </div>
-                  <div class="flex">
-                    <span class="title-font font-medium text-2xl text-white">
-                      ${selectedProductItem.price}
-                    </span>
-                    <button class="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
-                      Buy Now
-                    </button>
-                    <button class="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                      <svg
-                        fill="currentColor"
-                        stroke-linecap="round"
-                        strokeLineJoin="round"
-                        strokeWidth="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                      </svg>
-                    </button>
+                    ))}
                   </div>
                 </div>
               </div>
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                  {selectedProductItem.brand}
+                </h2>
+                <h1 className="text-white text-3xl title-font font-medium mb-1">
+                  {selectedProductItem.title}
+                </h1>
+                <div className="flex mb-4">
+                  <span className="flex items-center">
+                    {[...Array(5)].map((_, index) => (
+                      <Icon key={index} />
+                    ))}
+                    <span className="ml-3">
+                      {selectedProductItem.rating.toFixed(1)} Rating
+                    </span>
+                  </span>
+                  <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-800 text-gray-500 space-x-2">
+                    {/* Your social media icons */}
+                  </span>
+                </div>
+                <p className="leading-relaxed">
+                  {selectedProductItem.description}
+                </p>
+                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5"></div>
+                <div className="flex">
+                  <span className="title-font font-medium text-2xl text-white">
+                    ${selectedProductItem.price}
+                  </span>
+                  <button className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
+                    Buy Now
+                  </button>
+                  <button className="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      strokeLineJoin="round"
+                      strokeWidth="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      {/* Your SVG path for the second button */}
+                    </svg>
+                  </button>
+                </div>
+              </div>
             </div>
-          </section>
-      }
+          </div>
+        </section>
+      )}
     </div>
   );
 };
